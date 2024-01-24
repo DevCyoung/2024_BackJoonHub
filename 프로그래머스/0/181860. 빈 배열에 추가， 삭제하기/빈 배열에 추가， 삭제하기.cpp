c6f774eef43fd7d52cb432489f58f8451422d645
DevCyoung <tkdlqldjq123@gmail.com>
2024-01-25 06:43:46 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+//X[] arr[] flag[]
+//
+
+vector<int> solution(vector<int> arr, vector<bool> flag) 
+{
+    vector<int> answer;
+    
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        if (flag[i])
+        {
+            for (int j = 0; j < arr[i] * 2; ++j)
+            {
+                answer.push_back(arr[i]);
+            }
+        }
+        else
+        {
+            for (int j = 0; j < arr[i]; ++j)
+            {
+                answer.pop_back();
+            }
+        }
+    }
+    
+    return answer;
+}

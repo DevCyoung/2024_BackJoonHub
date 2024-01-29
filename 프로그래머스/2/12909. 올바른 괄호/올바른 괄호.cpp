c4cf51eb5eb3a81a554940990269deec9c6dc6f4
DevCyoung <tkdlqldjq123@gmail.com>
2024-01-29 19:45:56 +0900

    [level 2] Title: 올바른 괄호, Time: 0.12 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,35 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;    
+    int stack = 0;
+    
+    for (char item : s)
+    {
+        if ( '(' == item)
+        {
+            ++stack;
+        }
+        else
+        {
+            --stack;
+        }
+        
+        if (stack < 0)
+        {
+            answer = false;
+            break;
+        }
+    }
+    
+    if (stack)
+    {
+        answer = false;
+    }
+
+    return answer;
+}

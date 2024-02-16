7fe5f88f7bea05e12c16fd73214a3177f991c9d3
DevCyoung <tkdlqldjq123@gmail.com>
2024-02-16 16:24:33 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -6,31 +6,17 @@ using namespace std;
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
     map<string, int> check;
-    
     for (int i = 0; i <= discount.size() -10; ++i)
     {
         check.clear();
         for (int j = 0; j < 10; ++j)
-        {
-            ++check[discount[i + j]];
-        }
-        
+            ++check[discount[i + j]];        
         bool flag = true;
-        for (int j = 0; j < number.size(); ++j)
-        {
-            if (number[j] > check[want[j]])
-            {
-                flag = false;
-                break;
-            }
-        }
-        
-        if (flag)
-        {
-            ++answer;
-        }
-        
+        for (int j = 0; j < number.size(); ++j)    
+            if (number[j] != check[want[j]])            
+                flag = false;                                            
+        if (flag)    
+            ++answer;                
     }
-    
     return answer;
 }

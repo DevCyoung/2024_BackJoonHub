f9d522c21ec5cc4dee507906811b5fa5ce5ccf3b
DevCyoung <tkdlqldjq123@gmail.com>
2024-02-18 14:17:33 +0900

    [level 2] Title: 호텔 대실, Time: 0.54 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -7,46 +7,36 @@ using namespace std;
 
 int toMinute(const string& str)
 {
-    int minute = 0;
+    int hour   = stoi(str.substr(0, 2));
+    int minute = stoi(str.substr(3, 2));
     
-    minute = str[0] - '0';
-    minute = minute * 10 + (str[1] - '0');
-    minute *= 60;
-    
-    minute += (str[3] - '0') * 10;
-    minute += (str[4] - '0');
-
-    return minute;
+    return hour * 60 + minute;
 }
 
 int solution(vector<vector<string>> book_time) {
     int answer = 0;
-    vector<pair<int, int>> time_table;
     
-    for (const vector<string>& books : book_time)
-    {
-        time_table.push_back({toMinute(books[0]), toMinute(books[1])});
-    }
-    
-    sort(time_table.begin(), time_table.end());    
+    sort(book_time.begin(), book_time.end());    
     vector<int> rooms;
-    
-    for (auto& times : time_table)
+
+    for (auto& times : book_time)
     {                
-        bool flag = true;
+        bool find_room = false;
+        int start_time = toMinute(times[0]);
+        int end_time = toMinute(times[1]);
         for (int i = 0; i < rooms.size(); ++i)
         {
-            if (rooms[i] + 10 <= times.first)
+            if (rooms[i] + 10 <= start_time)
             {
-                flag = false;
-                rooms[i] = times.second;
+                find_room = true;
+                rooms[i] = end_time;
                 break;
             }
         }
-        
-        if (flag)
+
+        if (!find_room)
         {
-            rooms.push_back(times.second);            
+            rooms.push_back(end_time);
         }
     }
 

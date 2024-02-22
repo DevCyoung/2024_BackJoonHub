5b393b6763f5a942dc3bba11032fca141d2ac883
DevCyoung <tkdlqldjq123@gmail.com>
2024-02-22 19:57:23 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
@@ -1,102 +1,67 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-vector<string> pitchSplit(string str)
+string convertSharp(string notes)
 {
-    vector<string> result;
-    
-    for (int i = 0; i < str.size(); ++i)
+    string ret = "";
+    const int diff = 'A' - 'a';
+    for(int i=0; i<notes.size(); i++)
     {
-        string temp = "";
-        
-        temp += str[i];
-        if (i + 1 < str.size() && str[i + 1] == '#')
+        if(i < notes.size()-1 && notes.at(i+1) == '#')
         {
-            temp += str[i + 1];
-            ++i;
+            ret.push_back((char)(notes.at(i) - diff));
+            i++;
         }
-        result.push_back(temp);        
-    }
-    
-    return result;
-}
-
-vector<string> comSplit(string str)
-{
-    vector<string> result;
-    
-    for (int i = 0; i < str.size(); ++i)
-    {
-        string temp = "";
-        for (; i < str.size(); ++i)
+        else
         {
-            if (str[i] == ',')
-            {
-                break;
-            }
-            temp += str[i];
-        }        
-        result.push_back(temp);
-    }
-    return result;
-}
-
-int getTime(string time)
-{
-    int hour = std::stoi(time.substr(0, 2));
-    int miniute = std::stoi(time.substr(3, 2));
-    
-    return hour * 60 + miniute;
-}
-
-bool isThatMusic(const vector<string>& pitchs_m, const vector<string>& pitchs_musincInfo, const int time)
-{
-    for (int i = 0; i < time; ++i)
-    {
-        bool    flag     = true;        
-        int     tempTime = time - i;
-
-        for (int j = 0; j < pitchs_m.size(); ++j)
-        {
-            if (pitchs_m[j % pitchs_m.size()] != pitchs_musincInfo[(i + j) % pitchs_musincInfo.size()])
-            {
-                flag = false;
-                break;                
-            }                   
-            --tempTime;
-        }        
-        if (flag && tempTime >= 0)
-        {
-            return true;
+            ret.push_back(notes.at(i));
         }
     }
-    
-    return false;
+    return ret;
 }
+
+string getNotesToFind(string info, int countToFind, string& title)
+{
+    string ret = "";
+
+    const int maxTime = 24 * 60;
+    int begin = stoi(info.substr(0, 2))*60 + stoi(info.substr(3, 2));
+    int end = stoi(info.substr(6, 2))*60 + stoi(info.substr(9, 2));
+    int runLength = begin >= maxTime? 0 : min(maxTime, end) - begin;
+    if(countToFind > runLength)
+        return ret;
+
+    title = info.substr(12, info.rfind(',')-12);
+    int notePtr = 13 + title.size();
+    int noteLength = info.size() - notePtr;
+    string notes = convertSharp(info.substr(notePtr, noteLength));
+    noteLength = notes.size();
+    for(int r=0; r<runLength / noteLength; r++)
+    {
+        ret = ret.append(notes);
+    }
+    ret = ret.append(notes, 0, runLength % noteLength);
+    return ret;
+}
+
 
 string solution(string m, vector<string> musicinfos) {
     string answer = "(None)";
-    int tempTime = 0;
-    
-    for (string musicInfo: musicinfos)
-    {        
-        vector<string> infos                =  comSplit(musicInfo);        
-        vector<string> pitchs_m             =  pitchSplit(m);
-        vector<string> pitchs_musicncInfo   =  pitchSplit(infos[3]);
-        int time                            =  getTime(infos[1]) - getTime(infos[0]);
-        
-        if (isThatMusic(pitchs_m, pitchs_musicncInfo, time))
+
+    int length = 0;
+    for(auto it = musicinfos.begin(); it != musicinfos.end(); it++)
+    {
+        string title;
+        string fnotes = convertSharp(m);
+        string notes = getNotesToFind(*it, fnotes.size(), title);
+        if(notes.find(fnotes) != string::npos && notes.size() > length)
         {
-            if (tempTime < time)
-            {
-                tempTime = time;
-                answer = infos[2];
-            }
+            answer = title;
+            length = notes.size();
         }
-    }    
-    
+    }
+
     return answer;
 }

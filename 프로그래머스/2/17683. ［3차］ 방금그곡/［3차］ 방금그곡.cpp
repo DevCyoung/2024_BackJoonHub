0b53ffdaa1b83cabb59bb70102e6b5b481872418
DevCyoung <tkdlqldjq123@gmail.com>
2024-02-22 19:31:33 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.02 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
@@ -0,0 +1,121 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<string> musicSplit(string str)
+{
+    vector<string> result;
+    
+    for (int i = 0; i < str.size(); ++i)
+    {
+        string temp = "";
+        
+        temp += str[i];
+        if (i + 1 < str.size() && str[i + 1] == '#')
+        {
+            temp += str[i + 1];
+            ++i;
+        }
+        result.push_back(temp);        
+    }
+    
+    return result;
+}
+
+vector<string> comSplit(string str)
+{
+    vector<string> result;
+    
+    for (int i = 0; i < str.size(); ++i)
+    {
+        string temp = "";
+        for (; i < str.size(); ++i)
+        {
+            if (str[i] == ',')
+            {
+                break;
+            }
+            temp += str[i];
+        }        
+        result.push_back(temp);
+    }
+    return result;
+}
+
+int getTime(string time)
+{
+    int hour = std::stoi(time.substr(0, 2));
+    int miniute = std::stoi(time.substr(3, 2));
+    
+    return hour * 60 + miniute;
+}
+
+bool isThatMusic(const vector<string>& pitchs_m, const vector<string>& pitchs_musincInfo, const int time)
+{
+    int i = 0;
+    while (i < time)
+    {
+        bool flag = true;
+        int j = 0;
+        int k = 0;
+        int tempTime = time - i;
+
+        while (j < pitchs_m.size())
+        {
+            //std::cout << pitchs_musincInfo[(i + k) % pitchs_musincInfo.size()];
+            if (pitchs_musincInfo[(i + k) % pitchs_musincInfo.size()] != pitchs_m[j % pitchs_m.size()])
+            {
+                flag = false;
+                break;                
+            }            
+
+            ++j;
+            ++k;
+            --tempTime;
+        }
+        
+        //std::cout << std::endl;
+        
+        if (flag && tempTime >= 0)
+        {
+            return true;
+        }
+        
+        ++i;
+    }
+    
+    return false;
+}
+
+string solution(string m, vector<string> musicinfos) {
+    string answer = "(None)";
+    int tempTime = 0;
+    
+    for (string musicInfo: musicinfos)
+    {        
+        vector<string> infos                = comSplit(musicInfo);        
+        vector<string> pitchs_m             =  musicSplit(m);
+        vector<string> pitchs_musicncInfo   =  musicSplit(infos[3]);
+        int time = getTime(infos[1]) - getTime(infos[0]);
+        
+        //for (int i = 0; i < pitchs_musicncInfo.size(); ++i)
+        //{
+        //    std::cout <<  pitchs_musicncInfo[i] << " ";
+        //}
+        //std::cout << std::endl;
+            
+        if (isThatMusic(pitchs_m, pitchs_musicncInfo, time))
+        {
+            if (tempTime < time)
+            {
+                tempTime = time;
+                answer = infos[2];
+            }
+           // std::cout << infos[2] << std::endl;
+        }
+    }    
+    
+    return answer;
+}

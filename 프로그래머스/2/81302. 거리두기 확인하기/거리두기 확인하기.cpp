1ca6c79ef2b549a2ea89956664891cd7db06071e
DevCyoung <tkdlqldjq123@gmail.com>
2024-02-26 03:21:50 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
@@ -1,60 +1,40 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <cstring>
 
 using namespace std;
 
-bool bfs(const int x, const int y, const vector<string>& place)
+bool isSafe(vector<string> place)
 {
-    int dx[4] = {0, 1,  0,  -1};
-    int dy[4] = {1, 0, -1,   0};    
-    bool visited[5][5];
-    queue<pair<int,int>> q;
-    
-    q.push({x, y});
-    memset(visited, false, sizeof(visited));            
-    visited[y][x] = true;
-    
-    while (!q.empty())
+    int dx[4] = {0, 1,  0, -1};
+    int dy[4] = {1, 0, -1,  0};
+    for (int y = 0; y < 5; ++y)
     {
-        pair<int, int> pos = q.front();
-        q.pop();
-        for (int i = 0; i < 4; ++i)
+        for (int x = 0; x < 5; ++x)
         {
-            int nx = pos.first  + dx[i];
-            int ny = pos.second + dy[i];
-            if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || visited[ny][nx])
-                continue;
-            else if (abs(nx - x) + abs(ny - y) > 2 || place[ny][nx] == 'X')
-                continue;                        
-            if (place[ny][nx] == 'P')
+            if (place[y][x] == 'P')
             {
-                return false;
+                for (int i = 0; i < 4; ++i)
+                {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    
+                    if (nx <0 || ny < 0 || nx >=5 || ny >= 5 || place[ny][nx] == 'X')
+                        continue;
+                    if (place[ny][nx] == 'B' || place[ny][nx] == 'P')
+                        return false;
+                    place[ny][nx] = 'B';
+                }
             }
-            visited[ny][nx] = true;
-            q.push({nx, ny});
         }
     }
     return true;
 }
-bool isSafe(vector<string> place)
-{
-    for (int y = 0; y < 5; ++y)
-    {        
-        for (int x = 0; x < 5; ++x)
-        {
-            if (place[y][x] != 'P')
-                continue;
-            if (false == bfs(x, y, place))
-                return false;      
-        }        
-    }
-    return true;
-}
+
 vector<int> solution(vector<vector<string>> places) {
     vector<int> answer;
-    for (vector<string>& place : places)
-        answer.push_back(isSafe(place));        
+    for (vector<string> place : places)
+    {
+        answer.push_back(isSafe(place));
+    }
     return answer;
 }

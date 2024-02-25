b505da1e0184ab5d3f732414c1a9a5e566a65e61
DevCyoung <tkdlqldjq123@gmail.com>
2024-02-26 00:26:49 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -1,11 +1,17 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
 using namespace std;
 
-long long solution(int w, int h) {
-    long long answer = 0;
-
-    for (int i = 0; i < w; ++i) {
-        answer += (int)((double)h*i/w);
+long long solution(int w,int h) 
+{
+    long long scale = (long long)w * (long long)h;
+    double inclination = (double)max(w, h) / (double)min(w,h);
+    long long step = (long long)inclination;    
+    if (floor(inclination) > 0)
+    {
+        ++step;        
     }
-
-    return 2 * answer;
+    return scale - step * min(w, h);
 }

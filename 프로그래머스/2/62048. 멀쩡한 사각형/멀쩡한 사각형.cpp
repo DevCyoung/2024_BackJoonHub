716bf80f978b10cf70d94690cc38754f16578de4
DevCyoung <tkdlqldjq123@gmail.com>
2024-02-26 00:08:36 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -3,22 +3,17 @@
 #include <iostream>
 
 using namespace std;
+int gcd(int a, int b)
+{
+    if (a == 0)
+    {
+        return b;
+    }
 
+    return gcd(b % a, a);
+}
 long long solution(int w,int h) 
 {
-    long long answer = 0;
-    if (w > h)
-    {
-        int temp = h;
-        h  = w;
-        w = temp;
-    }
-    
-    for (long long x = 0; x < w; ++x)        
-    {     
-        long long ly = x * h / w;
-        long long ry = (h*(x+1) + w - 1) / w;
-        answer += ry - ly;
-    }    
-    return (long long)w * (long long)h - answer;
+    long long size = (long long)w + (long long)h - gcd(w, h);
+    return (long long)w * (long long)h - size;
 }

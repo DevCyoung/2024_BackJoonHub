5a496faf2932b4d3e78c564789b12180b2869dd7
DevCyoung <tkdlqldjq123@gmail.com>
2024-02-26 10:30:00 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+bool check(int a, int b)
+{
+    long long alens[3] = {a * 2, a * 3, a * 4};    
+    long long blens[3] = {b * 2, b * 3, b * 4};
+    
+    for (long long i = 0; i < 3; ++i)
+    {
+        for (long long j = 0; j < 3; ++j)
+        {
+            if (alens[i] == blens[j])
+                return true;
+        }
+    }
+        
+    return false;
+}
+
+long long solution(vector<int> weights) {
+    long long answer = 0;
+    long long weight_counts[1001] = {0,};
+    for (long long i = 0; i < weights.size(); ++i)
+        ++weight_counts[weights[i]];
+    
+    for (int i = 0; i < 1001; ++i)
+    {
+        if (weight_counts[i] == 0)        
+            continue;        
+        else if (weight_counts[i] > 1)
+            answer += (weight_counts[i] * (weight_counts[i] -1)) / 2;
+        
+        for (int j = i + 1; j < 1001; ++j)
+        {
+            if (weight_counts[j] == 0)
+                continue;            
+            if (check(i, j))
+                answer += weight_counts[i] * weight_counts[j];
+        }
+    }
+    return answer;
+}

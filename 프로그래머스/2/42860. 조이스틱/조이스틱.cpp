09e01f844e2a657f556584715e7b7c8eb6211041
DevCyoung <tkdlqldjq123@gmail.com>
2024-03-29 09:37:24 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+int get_min_move(const int step_dir, const int back_dir, const string& name)
+{
+    int cursor = 0;
+    int move_count = 0;
+    int min = INT_MAX;    
+    string hi_score_name(name.size(), 'A');   
+    
+    while (hi_score_name != name)
+    {   
+        if (hi_score_name[cursor] != name[cursor])
+        {            
+            hi_score_name[cursor] = name[cursor];
+        }                
+        string temp = hi_score_name;
+        int move_count_temp = move_count;
+        int temp_cursor = cursor;        
+        while (temp != name)
+        {
+            temp_cursor += back_dir;
+            ++move_count_temp;
+            
+            if (temp_cursor < 0)
+            {
+                temp_cursor = name.size() - 1;
+            }
+            else if (temp_cursor >= name.size())
+            {
+                temp_cursor = 0;
+            }
+            
+            temp[temp_cursor] = name[temp_cursor];
+        }
+        
+        if (move_count_temp < min)
+        {
+            min = move_count_temp;
+        }
+                
+        cursor += step_dir;
+        ++move_count;
+        
+        if (cursor < 0)
+        {
+            cursor = name.size() - 1;
+        }
+        else if (cursor >= name.size())
+        {
+            cursor = 0;
+        }                
+    }   
+    
+    
+    if (min == INT_MAX)
+    {
+        min = 0;
+    }
+    
+    return min;
+}
+
+int solution(string name) {
+    int answer = 0;
+    
+    for (char item : name)
+    {
+        answer += item - 'A' < 'Z' - item + 1 ? item - 'A' : 'Z' - item + 1;
+    }
+    int min1 = get_min_move(1,  -1, name);
+    int min2 = get_min_move(-1,  1, name);        
+    answer += min1 < min2 ? min1 : min2;    
+    return answer;
+}

b7317470f3a203d681e38aea91ed93d3ed7cc100
DevCyoung <tkdlqldjq123@gmail.com>
2024-03-30 09:33:54 +0900

    [level 2] Title: 요격 시스템, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -7,28 +7,12 @@ using namespace std;
 
 bool compare(std::vector<int>& a, std::vector<int>& b)
 {
-    if (a[0] < b[0])
-    {
-        return true;
-    }    
-    else if (a[0] == b[0] && a[1] < b[1])
-    {        
-        return true;
-    }
-    
-    return false;
+    return a[1] < b[1];
 }
 
 int solution(vector<vector<int>> targets) {
     int answer = 0;
     sort(targets.begin(), targets.end(), compare);
-    
-    // for (auto& item : targets)
-    // {
-    //     std::cout << item[0] << "," << item[1] << " ";
-    // }
-    // std::cout << std::endl;
-    
     int idx = 0;
     while (idx < targets.size())
     {
@@ -39,13 +23,10 @@ int solution(vector<vector<int>> targets) {
             if (pick[1] <= targets[i][0])
             {
                 break;
-            }
-            
-            pick[1] = std::min(pick[1], targets[i][1]);            
-            //pick = targets[i];
+            }            
+            pick[1] = std::min(pick[1], targets[i][1]);
             ++idx;
         }        
     }
-    
     return answer;
 }
